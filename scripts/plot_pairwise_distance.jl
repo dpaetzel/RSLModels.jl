a18b802515b3244a8baf2203c98faf5a381a850d
David Pätzel <david.paetzel@posteo.de>
2023-09-20 11:14:51 +0200

    📄 Add license

--- scripts/plot_pairwise_distance.jl
+++ scripts/plot_pairwise_distance.jl
@@ -1,3 +1,23 @@
+# plot_pairwise_distance.jl plots pairs of intervals and data
+#
+# It is intended to be used for sanity checking different data-dependent ways to
+# measure interval distance.
+#
+# Copyright (C) 2023 David Pätzel
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # We want to track global assignments as well when doing
 # `includet(thisfile.jl)`.
 __revise_mode__ = :evalassign
