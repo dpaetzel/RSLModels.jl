a6b40d915522ce0eded6e80b9285392332cb5301
David Pätzel <david.paetzel@posteo.de>
2023-10-10 12:18:11 +0200

    ✨ Serialize to disk the results of `genstats.jl`

--- scripts/genstats.jl
+++ scripts/genstats.jl
@@ -1,6 +1,7 @@
 using Base.Filesystem
 using Comonicon
 using ProgressBars
+using Serialization
 using Statistics
 
 using RSLModels.LocalModels
@@ -26,7 +27,14 @@ the learning tasks to disk.
     n::Int=200,
     seed::Int=0,
     prefix_fname::String="data/$d-$k-$n-$seed",
+    pbar=missing,
 )
+    if ismissing(pbar)
+        myprintln = println
+    else
+        myprintln = s -> println(pbar, s)
+    end
+
     params = Dict(:d => d, :k => k, :seed => seed)
     task = generate(d, k, n; seed=seed)
     match_X = task.match_X
@@ -41,8 +49,14 @@ the learning tasks to disk.
         :mae_test => mean(abs.(task.y_test .- y_test_pred)),
     )
 
+    data = Dict(:params => params, :stats => stats)
+
     # TODO Mlflow here
-    println("$d $k $n $seed => $stats")
+    myprintln("$d $k $n $seed => $stats")
+
+    mkpath(dirname(prefix_fname))
+    serialize("$prefix_fname.jls", data)
+
     # TODO Consider to store model (but not generated data)
 
     return nothing
@@ -84,21 +98,27 @@ the task and the sample to disk.
 - `--endseed`:
 - `--prefix-fname`:
 """
-@cast function genall(; startseed::Int=0, endseed::Int=9)
-    for (seed, d, k) in ProgressBar(
+@cast function genall(;
+    startseed::Int=0,
+    endseed::Int=9,
+    prefix_fname::String="data/genstats/genall",
+)
+    iter = ProgressBar(
         Iterators.product(
             startseed:endseed,
             [1, 2, 3, 5, 10, 20],
             [2, 4, 7, 10, 15, 20],
         ),
     )
+    for (seed, d, k) in iter
         n = Int(round(200 * 10^(d / 5)))
         gen(;
             d=d,
             k=k,
             n=n,
             seed=seed,
-            prefix_fname="data/genall/$d-$k-$n-$seed",
+            prefix_fname="$prefix_fname/$d-$k-$n-$seed",
+            pbar=iter,
         )
     end
 end
