b3a09b8d4aa3b24a79f451910db969c02b22f61f
David Pätzel <david.paetzel@posteo.de>
2023-11-10 15:26:00 +0100

    ✨ Update drawdata.jl

--- scripts/drawdata.jl
+++ scripts/drawdata.jl
@@ -1,11 +1,13 @@
 using AlgebraOfGraphics
 using Base.Filesystem
 using CairoMakie
+using CSV
 using DataFrames
 using Dates
 using KittyTerminalImages
 using LaTeXStrings
 using ProgressBars
+using Random
 using Serialization
 using Statistics
 using StatsBase
@@ -15,230 +17,188 @@ using RSLModels.Tasks
 using RSLModels.Utils
 using RSLModels.Plots
 
-# bins = [0.7, 0.8, 0.9, 0.95]
-bins = [0.7, 0.8, 0.9]
-
 # We aim at having this many learning tasks per combination of d, K and coverage bin.
 n_per_d_k_coverage = 4
-Ks = [3, 5, 7, 10, 15, 20, 30]
+
+fname_paramselect = "2023-11-09-18-35-32-kdata.paramselect.csv"
+println("Reading parameter selection from $fname_paramselect …")
+df_selparams = DataFrame(CSV.File(fname_paramselect))
+
+dname_stats = "../2023-11-10-data-merged"
+println("Reading data set statistics from $dname_stats …")
+df_stats = readstats(; prefix_fname=dname_stats)
+
+df_stats_K2 = df_stats[df_stats[:, "stats.K"] .== 2, :]
+if all(df_stats_K2[:, "stats.overlap_pairs_mean_per_rule"] .== 0)
+    println(
+        "$mjgood All $(nrow(df_stats_K2)) sets with K=2 have an overlap of 0, as expected.",
+    )
+end
+
+bins = sort(unique(df_selparams.rate_coverage_min))
+
+# Filter for `coverage >= 0.7` (may not be fulfilled by a margin due to coverage
+# computation being sample-based but we have enough data to just discard these
+# cases).
+# TODO Not 100% sure whether we want to do this
+gt = df_stats[!, "stats.coverage"] .>= bins[1]
+println(
+    "Discarding $(size(df_stats, 1) - count(gt)) of $(size(df_stats, 1)) rows where " *
+    "coverage is smaller than lowest bin …",
+)
+df_stats = df_stats[gt, :]
+
+# Add a column denoting the respective coverage bin.
+df_stats[!, "stats.coverage_bin"] = map(
+    coverage -> bins[searchsortedlast(bins, coverage)],
+    df_stats[:, "stats.coverage"],
+)
+
+n_DXs = length(unique(df_stats[:, "params.d"]))
+n_coverage = length(bins)
+Ks = sort(unique(df_stats[:, "params.K_target"]))
+n_Ks = length(Ks)
+n_sel = n_DXs * n_coverage * n_Ks * n_per_d_k_coverage
+println(
+    "Overall, we want to select (n_DXs=$n_DXs) * (n_coverage=$n_coverage) " *
+    "* (n_Ks=$n_Ks) * (n_per_d_k_coverage=$n_per_d_k_coverage) = $n_sel of " *
+    "the remaining $(size(df_stats, 1)) learning tasks.",
+)
 
 mapping_grid = mapping(;
     row="stats.coverage_bin" => nonnumeric,
     col="params.d" => nonnumeric,
 )
 
-function analyse(;
-    folder="2023-10-27-data-full-12d",
-    bins=bins,
-    n_per_d_k_coverage=n_per_d_k_coverage,
-    Ks=Ks,
+df = df_stats
+
+println("This is the distribution of the K's.")
+display(draw(data(df) * frequency() * mapping("stats.K")))
+println()
+
+println("These are the frequencies of the combinations.")
+println(
+    "Note that we will try in the next step to draw from each of " *
+    "these bins for each K in $Ks $n_per_d_k_coverage learning tasks.",
 )
-    df = readstats(; prefix_fname=folder)
-
-    println("$mjgood Read $(size(df, 1)) data set statistics from $folder.")
-
-    if all(
-        df[df[:, "stats.K"] .== 2, :][
-            :,
-            "stats.overlap_pairs_mean_per_rule",
-        ] .== 0,
-    )
-        println("$mjgood All sets with K=2 have an overlap of 0, as expected.")
-    end
-
-    # Check whether all is dandy.
-    println("Look at this for any weirdnesses:")
-    display(
-        combine(
-            groupby(df, ["params.d", "params.nif"]),
-            # Note that we select any column here (`fnames`) and count how many
-            # values it has in order to be able to rename the output column
-            # within the same expression.
-            "fname" => (df_ -> size(df_, 1)) => "n_datasets",
-        ),
-    )
-
-    # Filter for `coverage >= 0.7` (may not be fulfilled by a margin due to coverage
-    # computation being sample-based but we have enough data to just discard these
-    # cases).
-    gt = df[!, "stats.coverage"] .>= bins[1]
-    println(
-        "\n" *
-        "Discarding $(size(df, 1) - count(gt)) of $(size(df, 1)) rows where " *
-        "coverage is smaller than lowest bin …",
-    )
-    df = df[gt, :]
-
-    # Add a column denoting the respective coverage bin.
-    df[!, "stats.coverage_bin"] = map(
-        coverage -> bins[searchsortedlast(bins, coverage)],
-        df[:, "stats.coverage"],
-    )
-
-    n_dims = length(unique(df[:, "params.d"]))
-    n_overlap = length(bins)
-    n_Ks = length(Ks)
-    n_sel = n_dims * n_overlap * n_Ks * n_per_d_k_coverage
-    println(
-        "\n" *
-        "Overall, we want to select (n_dims=$n_dims) * (n_overlap=$n_overlap) " *
-        "* (n_Ks=$n_Ks) * (n_per_d_k_coverage=$n_per_d_k_coverage) = $n_sel of " *
-        "the remaining $(size(df, 1)) learning tasks.",
-    )
-
-    println("This is the distribution of the K's.")
-    display(draw(data(df) * frequency() * mapping("stats.K")))
-
-    println("\n" * "These are the frequencies of the combinations.")
-    println(
-        "\n" *
-        "Note that we will try in the next step to draw from each of " *
-        "these bins for each K in $Ks $n_per_d_k_coverage learning tasks.",
-    )
-    display(
-        draw(
-            data(df) *
-            frequency() *
-            mapping(
-                "params.d" => nonnumeric,
-                "stats.coverage_bin" => nonnumeric;
-                # TODO have to bin this first
-                # layout="stats.K" => nonnumeric,
-            ),
-        ),
-    )
-    println(
-        "\n" * "If I only keep `\"stats.K\" <= 30` and bin K to resolution 5:",
-    )
-    display(
-        draw(
-            data(df[df[:, "stats.K"] .<= 30, :]) *
-            frequency() *
-            mapping(
-                "params.d" => nonnumeric,
-                "stats.coverage_bin" => nonnumeric;
-                # TODO have to bin this first
-                layout="stats.K" => nonnumeric ∘ (x -> round(x / 5) * 5),
-            ),
-        ),
-    )
-
-    function draw_foreach_k(df; n_per_k=n_per_d_k_coverage, ks=Ks)
-        tasks = Dict(k => DataFrame() for k in ks)
-
-        # Go once over the DataFrame, shuffling its rows beforehand.
-        for row in eachrow(df[shuffle(1:size(df, 1)), :])
-            local K = row["stats.K"]
-
-            # Get the vector of tasks that we already selected for the `K` of the
-            # current row and if we have not already collected enough tasks for it, add
-            # the task to it.
-            if in(K, ks) && size(tasks[K], 1) < n_per_k
-                push!(tasks[K], row)
-            end
-        end
-
-        return tasks
-    end
-
-    # Select learning tasks for each combination of `DX` and `coverage_bin`.
-    df_sel = combine(
-        groupby(df, ["params.d", "stats.coverage_bin"]),
-        draw_foreach_k,
-    )
-    df_sel = flatten(df_sel, "x1")
-    # Extract `K` to its own column.
-    df_sel[!, "K"] = map(first, df_sel.x1)
-    # Extract the file names from the inner `DataFrame`s. The ternary is required
-    # to catch cases where the DataFrame is empty (since then indexing for "fname"
-    # fails) which can happen if not all data has been generated yet or if we
-    # discarded (see above) all of the tasks within one of the groups.
-    df_sel[!, "fname"] =
-        map(pair -> size(pair[2], 1) > 0 ? pair[2][!, "fname"] : [], df_sel.x1)
-    df_sel[!, "nif"] = map(
-        pair -> size(pair[2], 1) > 0 ? pair[2][!, "params.nif"] : [],
-        df_sel.x1,
-    )
-    # Flatten wrt to the file names and nif (which are vectors up until now).
-    df_sel = flatten(df_sel, "fname")
-    df_sel = flatten(df_sel, "nif")
-
-    # # Next make sure that all the combinations are in the DataFrame (we only
-    # # draw from existing combinations above).
-    # for dx in unique(df[:, "params.d"])
-    #     for coverage in bins
-    #         for K in Ks
-    #             df[:, "params.d"] ==
-    #         end
-    #     end
-    # end
-
-    # Print any combinations for which we have not yet created enough learning
-    # tasks.
-    println(
-        "\n" *
-        "Look at the following plot for configurations where we " *
-        "did not yet create enough learning tasks (not-yellow areas in the plot).",
-    )
-    display(
-        draw(
-            data(df_sel) *
-            frequency() *
-            mapping(
-                "params.d" => nonnumeric,
-                "stats.coverage_bin" => nonnumeric;
-                layout="K" => nonnumeric,
-            ),
-        ),
-    )
-
-    println("\n")
-    draw(
-        data(df_sel) *
-        frequency() *
-        mapping(
-            "nif" => nonnumeric;
-            row="stats.coverage_bin" => nonnumeric,
-            col="params.d" => nonnumeric,
-        ),
-    ) |> display
-
-    println("\n")
-    draw(data(df) * mapping_grid * mapping("stats.K") * frequency()) |> display
-
+display(
     draw(
         data(df) *
-        mapping_grid *
-        mapping("stats.K", "params.nif") *
-        histogram(),
-    ) |> display
+        frequency() *
+        mapping(
+            "params.d" => nonnumeric,
+            "stats.coverage_bin" => nonnumeric;
+            # TODO have to bin this first
+            # layout="stats.K" => nonnumeric,
+        ),
+    ),
+)
+println()
 
-    function write_fishscript(df_sel)
-        length_df_sel = size(df_sel, 1)
+display(
+    draw(
+        data(df) *
+        mapping(
+            "stats.K";
+            row="params.K_target" => nonnumeric,
+            col="params.d" => nonnumeric,
+            color="stats.coverage_bin" => nonnumeric,
+        ) *
+        histogram(; bins=30),
+    ),
+)
+println()
 
-        fnames_selected = df_sel.fname
-        # We replace colons with dashes so scp'ing is easier.
-        thetime = replace(string(now()), ":" => "-")
-        folder_sel = "$thetime-task-selection"
-        open("$thetime-copy_selection.fish", "w") do f
-            write(f, "#!/usr/bin/env fish\n\n")
-            write(f, "# CAREFUL! THIS FILE WAS AUTOGENERATED!\n\n")
-            write(f, "mkdir $folder_sel\n")
-            for fname in fnames_selected
-                fname_glob = replace(fname, "stats.jls" => "*")
-                write(f, "cp -vu $(fname_glob) $folder_sel\n")
-            end
-            return write(
-                f,
-                "echo\n",
-                "echo There should be $length_df_sel tasks and there are (math (ls $folder_sel | wc -l) / 3).\n",
-            )
+df_sel = DataFrame(;
+    DX=Int[],
+    rate_coverage_min=Float64[],
+    K=Int[],
+    candidates=DataFrame[],
+)
+for row in eachrow(df_selparams)
+    push!(
+        df_sel,
+        (
+            DX=row.DX,
+            rate_coverage_min=row.rate_coverage_min,
+            K=row.K,
+            candidates=subset(
+                df,
+                "params.d" => dx -> dx .== row.DX,
+                "stats.K" => K -> K .== row.K,
+                "stats.coverage_bin" => rcm -> rcm .== row.rate_coverage_min,
+            ),
+        ),
+    )
+end
+
+df_sel[!, :count] = nrow.(df_sel.candidates)
+
+display(
+    draw(
+        data(df_sel) *
+        mapping(
+            :K,
+            :count;
+            row=:rate_coverage_min => nonnumeric,
+            col=:DX => nonnumeric,
+        ) *
+        visual(ScatterLines),
+    ),
+)
+println()
+
+function sampleupto(collection, n)
+    if size(collection, 1) < n
+        return collection
+    else
+        return sample(collection, n; replace=false)
+    end
+end
+
+df_sel[!, :selected] =
+    DataFrame.(sampleupto.(eachrow.(df_sel.candidates), n_per_d_k_coverage))
+df_sel[!, :count_selected] = nrow.(df_sel.selected)
+
+display(
+    draw(
+        data(df_sel) *
+        mapping(
+            :K,
+            :count_selected;
+            row=:rate_coverage_min => nonnumeric,
+            col=:DX => nonnumeric,
+        ) *
+        visual(ScatterLines),
+    ),
+)
+println()
+
+df_sel[!, :fnames] = getproperty.(df_sel.selected, "fname")
+
+function write_fishscript(df_sel)
+    length_df_sel = nrow(df_sel)
+
+    fnames_selected = reduce(vcat, df_sel.fnames)
+    # We replace colons with dashes so scp'ing is easier.
+    thetime = replace(string(now()), ":" => "-")
+    folder_sel = "$thetime-task-selection"
+    open("$thetime-copy_selection.fish", "w") do f
+        write(f, "#!/usr/bin/env fish\n\n")
+        write(f, "# CAREFUL! THIS FILE WAS AUTOGENERATED!\n\n")
+        write(f, "mkdir $folder_sel\n")
+        for fname in fnames_selected
+            fname_glob = replace(fname, "stats.jls" => "*")
+            write(f, "cp -vu $(fname_glob) $folder_sel\n")
         end
-
-        return nothing
+        return write(
+            f,
+            "echo\n",
+            "echo There should be $length_df_sel tasks and there are (math (ls $folder_sel | wc -l) / 3).\n",
+        )
     end
 
-    # return write_fishscript(df_sel)
-
-    return df
+    return nothing
 end
+
+write_fishscript(df_sel)
