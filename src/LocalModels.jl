90e4532e26ee32963bad0b0d92c2a1d840eb7d37
David Pätzel <david.paetzel@posteo.de>
2023-12-13 15:44:56 +0100

    🎨 Merge ConstantModel.{coef,dist_noise}

--- src/LocalModels.jl
+++ src/LocalModels.jl
@@ -17,31 +17,28 @@ inputs (i.e. only the number of inputs matters for this function).
 """
 
 @auto_hash_equals struct ConstantModel
-    coef::Real
-    dist_noise::Distribution
+    dist_out::Distribution
     coef_mix::Real
     # Whether this is a default rule (and should thus not undergo mixing
     # coefficient fixing).
     isdefault::Bool
     function ConstantModel(
-        coef::Real,
-        dist_noise::Distribution,
+        dist_out::Distribution,
         coef_mix::Real,
         # Whether this is a default rule (and should thus not undergo mixing
         # coefficient fixing).
         isdefault::Bool,
     )
-        return new(coef, dist_noise, coef_mix, isdefault)
+        return new(dist_out, coef_mix, isdefault)
     end
     function ConstantModel(
-        coef::Real,
-        dist_noise::Distribution,
+        dist_out::Distribution,
         coef_mix::Real;
         # Whether this is a default rule (and should thus not undergo mixing
         # coefficient fixing).
         isdefault::Bool=false,
     )
-        return new(coef, dist_noise, coef_mix, isdefault)
+        return new(dist_out, coef_mix, isdefault)
     end
 end
 
@@ -50,23 +47,22 @@ function draw_constantmodel(isdefault::Bool=false)
 end
 
 function draw_constantmodel(rng::AbstractRNG; isdefault::Bool=false)
-    coef = rand(rng, Uniform(0.0, 1.0))
+    mean_out = rand(rng, Uniform(0.0, 1.0))
 
     # TODO Derive more sensible and less arbitrary min and max noise values,
     # e.g. based on coef distribution bounds
-    std_noise_min = 0.001
-    std_noise = rand(rng, Uniform(std_noise_min, 0.2))
+    std_noise_out_min = 0.001
+    std_noise_out = rand(rng, Uniform(std_noise_out_min, 0.2))
     # TODO Consider to fix coef_mix based on other rules
     if isdefault
         # If this is a default rule, set its mixing weight to the smallest
         # positive number.
         coef_mix = nextfloat(0.0)
     else
-        coef_mix = 1.0 / rand(rng, Uniform(std_noise, 1.0))
+        coef_mix = 1.0 / rand(rng, Uniform(std_noise_out, 1.0))
     end
     return ConstantModel(
-        coef,
-        Normal(0.0, std_noise),
+        Normal(mean_out, std_noise_out),
         coef_mix;
         isdefault=isdefault,
     )
@@ -84,7 +80,7 @@ end
 
 function AbstractModels.output(rng::AbstractRNG, model::ConstantModel)
     # Note that outputs of constant models do not depend on inputs.
-    return model.coef + rand(rng, model.dist_noise)
+    return rand(rng, model.dist_out)
 end
 
 """
@@ -139,7 +135,8 @@ function AbstractModels.output_mean(
     model::ConstantModel,
     X::AbstractMatrix{Float64},
 )
-    return repeat([model.coef], size(X, 1))
+    mean_out = mean(model.dist_out)
+    return repeat([mean_out], size(X, 1))
 end
 
 """
