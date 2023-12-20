8dd76bf70d74686ea302f8f2d2f7ecaa49b33af6
David Pätzel <david.paetzel@posteo.de>
2023-12-20 11:18:22 +0100

    ✅ Add MLJ pipeline test

--- test/MLJInterface.jl
+++ test/MLJInterface.jl
@@ -2,10 +2,12 @@ using Logging
 using MLJ
 using Random
 using RSLModels.GARegressors
+using RSLModels.Transformers
 
 let
-    X, y = rand(300, 5), rand(300)
-    ffunc = GARegressors.mkffunc(GARegressors.MAEFitness(X, y))
+    Xmat, y = rand(300, 5), rand(300)
+    X = MLJ.table(Xmat)
+    ffunc = GARegressors.mkffunc(GARegressors.MAEFitness(Xmat, y))
     config = GARegressor(;
         x_min=0.0,
         x_max=1.0,
@@ -22,10 +24,28 @@ let
             config.rng = rng
 
             Logging.with_logger(SimpleLogger(Logging.Warn)) do
-                mach = machine(config, MLJ.table(X), y)
+                mach = machine(config, X, y)
                 fit!(mach)
                 return y_pred = predict(mach, X)
             end
+            # Right now we only test whether all types work out etc.
+            @test true
+        end
+    end
+
+    @testset "pipeline" begin
+        rng = Random.Xoshiro(123)
+
+        let config = deepcopy(config)
+            config.rng = rng
+
+            # Logging.with_logger(SimpleLogger(Logging.Warn)) do
+                #! format: off
+                mach = machine(MinMaxScaler() |> config, X, y)
+                #! format: on
+            fit!(mach)
+            y_pred = predict(mach, X)
+            # end
             # Right now we only test whether all types work out etc.
             @test true
         end
