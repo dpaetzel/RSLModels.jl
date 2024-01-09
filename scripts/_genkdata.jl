eca911259257f2c8dad22bf5d7641fdfb2f8c1c9
David Pätzel <david.paetzel@posteo.de>
2024-01-09 16:36:32 +0100

    ✨ Add explicit RNG to genkdata sampling

--- scripts/_genkdata.jl
+++ scripts/_genkdata.jl
@@ -1,19 +1,37 @@
 # This file is sent to all workers by genkdata.jl (which is itself a Comonicon
 # CLI app and cannot be sent to all workers straightforwardly).
 using Distributions
+using Random
 
 using RSLModels.Intervals
 
 function mysample1(DX, rate_coverage_min; usemmap=false, n_intervals_max=100)
+    return mysample1(
+        Random.default_rng(),
+        DX,
+        rate_coverage_min;
+        usemmap=usemmap,
+        n_intervals_max=n_intervals_max,
+    )
+end
+
+function mysample1(
+    rng::AbstractRNG,
+    DX,
+    rate_coverage_min;
+    usemmap=false,
+    n_intervals_max=100,
+)
     # TODO Consider to use a slightly informed distribution for `spread_min`.
     # spread_min = hist(rand(Beta(2, 2), 10000) / 2; bins=30)
-    spread_min = rand() / 2
+    spread_min = rand(rng) / 2
 
     # TODO Consider to invest time in choosing sensible ranges here
-    a = rand() * 199 + 1
-    b = rand() * 199 + 1
+    a = rand(rng) * 199 + 1
+    b = rand(rng) * 199 + 1
 
     rate_coverage, intervals = draw_intervals(
+        rng,
         DX;
         rate_coverage_min=rate_coverage_min,
         params_spread=(a=a, b=b),
