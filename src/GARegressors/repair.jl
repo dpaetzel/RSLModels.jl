2cf6e8120317f04e8d86542071c0078644f3d962
David Pätzel <david.paetzel@posteo.de>
2024-01-24 14:27:03 +0100

    🐛 Disable buggy repair operator

--- src/GARegressors/repair.jl
+++ src/GARegressors/repair.jl
@@ -13,24 +13,7 @@ function repair(
 end
 
 function repair(rng::AbstractRNG, g::Genotype, X::XType, nmatch_min::Int)
-    # TODO Consider to make this repair! and mutate g (i.e. deleteat!) directly
-    g_ = deepcopy(g)
-
-    k = nmatch_min
-
-    idx_rm = []
-    for idx in eachindex(g_)
-        if count(elemof(X, g_[idx])) < k
-            push!(idx_rm, idx)
-        end
-    end
-    deleteat!(g_, idx_rm)
-    n_removed = length(idx_rm)
-    if n_removed > 0
-        @debug "Removed $n_removed conditions due to less than $k training " *
-               "data matches" operator = "repair"
-    end
-
-    report = (; n_removed=n_removed)
-    return g_, report
+    # This does nothing for now.
+    report = (;)
+    return g, report
 end
